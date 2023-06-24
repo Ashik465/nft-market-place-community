2f51dea0c5adcecfc61a21a0c78902e0717a5135
Ashik465 <helloashikul@gmail.com>
2023-06-24 21:30:53 +0600

    layout done

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import router from './Routes/Route';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='container mx-auto'>
     <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
