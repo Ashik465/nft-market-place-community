16163babac2c7c9f1a015e2f4440107917386b80
Ashik465 <helloashikul@gmail.com>
2023-06-25 17:50:16 +0600

    navbar button done

--- src/pages/shared pages/navbar/Navbar.jsx
+++ src/pages/shared pages/navbar/Navbar.jsx
@@ -2,21 +2,36 @@ const Navbar = () => {
   const menu = (
     <>
       <li>
-        <a style={{ fontFamily: "DM Sans, sans-serif" }}className=" text-black text-base font-medium tracking-wide">Marketplace</a>
+        <a
+          style={{ fontFamily: "DM Sans, sans-serif" }}
+          className=" text-black text-base font-medium tracking-wide"
+        >
+          Marketplace
+        </a>
       </li>
 
       <li>
-        <a style={{ fontFamily: "DM Sans, sans-serif" }}className=" text-black text-base font-medium tracking-wide">Resource</a>
+        <a
+          style={{ fontFamily: "DM Sans, sans-serif" }}
+          className=" text-black text-base font-medium tracking-wide"
+        >
+          Resource
+        </a>
       </li>
       <li>
-        <a style={{ fontFamily: "DM Sans, sans-serif" }}className=" text-black text-base font-medium tracking-wide">About</a>
+        <a
+          style={{ fontFamily: "DM Sans, sans-serif" }}
+          className=" text-black text-base font-medium tracking-wide"
+        >
+          About
+        </a>
       </li>
     </>
   );
   return (
     <>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
+      <div className="navbar bg-base-100 border-b-[1px] border-[#EFEFEF] p-0 py-2 ">
+        <div className="navbar-start ">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
@@ -39,13 +54,11 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               {menu}
-
-              
             </ul>
           </div>
           <a
             style={{ fontFamily: "Roboto, sans-serif" }}
-            className="btn btn-ghost normal-case font-bold text-2xl text-[#3D00B7]"
+            className="btn btn-ghost px-0  normal-case font-bold text-xl md:text-4xl text-[#3D00B7]"
           >
             NFTERS
           </a>
@@ -53,8 +66,29 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{menu}</ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+        <div className="navbar-end space-x-2 md:space-x-4">
+      
+          <a
+            style={{
+              fontFamily: "DM Sans, sans-serif",
+              transition: "transform 0.2s ease-in-out",
+            }}
+            className=" btn-main"
+          >
+            Upload
+          </a>
+          <a
+            style={{
+              fontFamily: "DM Sans, sans-serif",
+              transition: "transform 0.2s ease-in-out",
+              whiteSpace: "nowrap",
+            }}
+            className=" btn-second-main"
+          >
+            Connect wallet
+          </a>
+
+       
         </div>
       </div>
     </>
