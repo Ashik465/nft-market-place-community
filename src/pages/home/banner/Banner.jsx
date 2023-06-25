23306a05b00a4635f9abcaa1b2d3962fbbaf42a3
Ashik465 <helloashikul@gmail.com>
2023-06-25 18:58:32 +0600

    banner half done

---
+++ src/pages/home/banner/Banner.jsx
@@ -0,0 +1,25 @@
+
+
+const Banner = () => {
+    return (
+        <>
+           <div className=" flex  mt-6 ">
+
+            <div className=" space-y-6">
+                  <h1
+                   style={{ fontFamily: "Roboto, sans-serif" }} 
+                   className="tracking-wide text-5xl text-black font-bold ">Discover, and collect Digital Art  NFTs </h1>  
+                  <p style={{
+              fontFamily: "DM Sans, sans-serif",
+            }} className=" text-[#565656] text-xl">Digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, Sell, and discover exclusive digital assets.</p>
+
+            <a className="btn-main">Explore Now</a>
+            </div>
+         
+            
+            </div> 
+        </>
+    );
+};
+
+export default Banner;
