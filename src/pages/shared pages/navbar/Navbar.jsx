80ad8eac68030dde2c8642cffacf88baffbeb6e8
Ashik465 <helloashikul@gmail.com>
2023-06-25 15:45:51 +0600

    menu done

--- src/pages/shared pages/navbar/Navbar.jsx
+++ src/pages/shared pages/navbar/Navbar.jsx
@@ -1,49 +1,64 @@
-
-
 const Navbar = () => {
-    return (
-        <>
-         <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a style={{ fontFamily: 'Roboto, sans-serif' }} className="btn btn-ghost normal-case font-bold text-xl text-[#3D00B7]">NFTERS</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
+  const menu = (
+    <>
+      <li>
+        <a style={{ fontFamily: "DM Sans, sans-serif" }}className=" text-black text-base font-medium tracking-wide">Marketplace</a>
       </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-        </>
-    );
+
+      <li>
+        <a style={{ fontFamily: "DM Sans, sans-serif" }}className=" text-black text-base font-medium tracking-wide">Resource</a>
+      </li>
+      <li>
+        <a style={{ fontFamily: "DM Sans, sans-serif" }}className=" text-black text-base font-medium tracking-wide">About</a>
+      </li>
+    </>
+  );
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {menu}
+
+              
+            </ul>
+          </div>
+          <a
+            style={{ fontFamily: "Roboto, sans-serif" }}
+            className="btn btn-ghost normal-case font-bold text-2xl text-[#3D00B7]"
+          >
+            NFTERS
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{menu}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
