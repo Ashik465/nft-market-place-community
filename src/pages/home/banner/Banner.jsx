c05155353fa86f03c684adba0734314f55df0fb4
Ashik465 <helloashikul@gmail.com>
2023-06-26 06:21:18 +0600

    banner done

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -4,7 +4,7 @@ import EffectCardsSlider from "./EffectCardsSlider";
 const Banner = () => {
   return (
     <>
-      <div className=" flex md:flex-row flex-col  gap-28  mt-6 ">
+      <div className=" flex md:flex-row flex-col gap-10  md:gap-28  mt-6 ">
         <div className=" p-4  md:w-1/2 space-y-4">
           <h1
             style={{ fontFamily: "Roboto, sans-serif" }}
@@ -16,7 +16,7 @@ const Banner = () => {
             style={{
               fontFamily: "DM Sans, sans-serif",
             }}
-            className=" text-[#565656] text-xl w-4/5"
+            className=" text-[#565656] text-xl md:w-4/5"
           >
             Digital marketplace for crypto collectibles and non-fungible tokens
             (NFTs). Buy, Sell, and discover exclusive digital assets.
--- src/pages/home/banner/EffectCardsSlider.jsx
+++ src/pages/home/banner/EffectCardsSlider.jsx
@@ -3,7 +3,7 @@ import bannerimg1 from "../../../assets/banner-image/img1.png";
 import bannerimg2 from "../../../assets/banner-image/img2.png";
 import bannerimg3 from "../../../assets/banner-image/img3.png";
 import people from "../../../assets/banner-image/people.png";
-
+import middle from "../../../assets/banner-image/middle.png";
 
 // swipper slider css
 import "./CardStyle.css";
@@ -33,11 +33,10 @@ const EffectCardsSlider = () => {
           <div
             style={{
               fontFamily: "DM Sans, sans-serif",
-              transition: "transform 0.2s ease-in-out",
             }}
-            className="relative z-10"
+            className="relative "
           >
-            <img  src={bannerimg1} alt="Banner Image 1" />
+            <img src={bannerimg1} alt="Banner Image 1" />
             <h1 className="absolute top-0 left-4 text-xl font-bold pt-3">
               Abstr Gradient NFT
             </h1>
@@ -47,19 +46,99 @@ const EffectCardsSlider = () => {
               alt=""
             />
             <p className="absolute  top-0 left-16 pt-12 ">Arkhan17</p>
+            {/* // bottom part */}
+            <div className="absolute  bottom-6 left-2  bg-[#FFFFFF33] rounded-lg backdrop-blur-xl p-5">
+              <div className="flex justify-between">
+                <div>
+                  <h1 className="text-xs ">Current Bid</h1>
+                  <div className="flex items-center justify-center gap-2">
+                    <img className="" src={middle} alt="" />
+                    <h1 className="text-base ">0.25 ETH</h1>
+                  </div>
+                </div>
 
-         
-            
+                <div className="ml-10">
+                  <h1 className="text-xs">Ends in</h1>
+
+                  <h1 className="text-base ">12h 43m 42s</h1>
+                </div>
+              </div>
+            </div>
+            {/* bottom part ends  */}
           </div>
         </SwiperSlide>
+        {/* 2nd slider image  */}
         <SwiperSlide>
-          <div>
-            <img  src={bannerimg2} alt="Banner Image 2"  />
+          <div style={{
+              fontFamily: "DM Sans, sans-serif",
+            }}
+            className="relative ">
+            <img src={bannerimg2} alt="Banner Image 2" />
+            <h1 className="absolute top-0 left-4 text-xl font-bold pt-3">
+              Orange Gradient NFT
+            </h1>
+            <img
+              className="absolute  top-0 left-4 pt-12 "
+              src={people}
+              alt=""
+            />
+            <p className="absolute  top-0 left-16 pt-12 ">Ashik</p>
+            {/* // bottom part */}
+            <div className="absolute  bottom-6 left-2  bg-[#FFFFFF33] rounded-lg backdrop-blur-xl p-5">
+              <div className="flex justify-between">
+                <div>
+                  <h1 className="text-xs ">Current Bid</h1>
+                  <div className="flex items-center justify-center gap-2">
+                    <img className="" src={middle} alt="" />
+                    <h1 className="text-base ">0.60 ETH</h1>
+                  </div>
+                </div>
+
+                <div className="ml-10">
+                  <h1 className="text-xs">Ends in</h1>
+
+                  <h1 className="text-base ">10h 30m 22s</h1>
+                </div>
+              </div>
+            </div>
+            {/* bottom part ends  */}
           </div>
         </SwiperSlide>
+        {/* third slider image  */}
         <SwiperSlide>
-          <div>
+          <div style={{
+              fontFamily: "DM Sans, sans-serif",
+            }}
+            className="relative ">
             <img src={bannerimg3} alt="Banner Image 3" />
+            <h1 className="absolute top-0 left-4 text-xl font-bold pt-3">
+              Red Gradient NFT
+            </h1>
+            <img
+              className="absolute  top-0 left-4 pt-12 "
+              src={people}
+              alt=""
+            />
+            <p className="absolute  top-0 left-16 pt-12 ">Ratul</p>
+            {/* // bottom part */}
+            <div className="absolute  bottom-6 left-2  bg-[#FFFFFF33] rounded-lg backdrop-blur-xl p-5">
+              <div className="flex justify-between">
+                <div>
+                  <h1 className="text-xs ">Current Bid</h1>
+                  <div className="flex items-center justify-center gap-2">
+                    <img className="" src={middle} alt="" />
+                    <h1 className="text-base ">0.50 ETH</h1>
+                  </div>
+                </div>
+
+                <div className="ml-10">
+                  <h1 className="text-xs">Ends in</h1>
+
+                  <h1 className="text-base ">11h 45m 22s</h1>
+                </div>
+              </div>
+            </div>
+            {/* bottom part ends  */}
           </div>
         </SwiperSlide>
       </Swiper>
