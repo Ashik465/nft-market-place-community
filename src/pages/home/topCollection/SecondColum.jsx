cee2860a7d1434e66e91d247e329e9f843aca6d4
Ashik465 <helloashikul@gmail.com>
2023-06-26 18:36:30 +0600

    second colum done

--- src/pages/home/topCollection/SecondColum.jsx
+++ src/pages/home/topCollection/SecondColum.jsx
@@ -1,11 +1,110 @@
-
+import ban2 from "../../../assets/top-collection/ban2.png";
+import people from "../../../assets/top-collection/people.png";
+import col2logo from "../../../assets/top-collection/col2.png";
 
 const SecondColum = () => {
-    return (
-        <>
-          second colum  
-        </>
-    );
+  return (
+    <>
+      <div className="px-10 md:px-0">
+        <div className="flex gap-2">
+          <img src={ban2} alt="" />
+          <div
+            style={{
+              fontFamily: "DM Sans, sans-serif",
+            }}
+            className=""
+          >
+            <h1 className=" font-bold text-xl">The Futr Abstr</h1>
+            <div className="flex items-center gap-2 mt-3">
+              <img src={people} alt="" />
+              <div className="flex gap-1 border-[1px] border-[#00AC4F] p-1 rounded-md">
+                <img src={col2logo} alt="" />
+                <p className=" text-[#00AC4F] font-bold text-xs md:text-base">
+                  0.25 ETH
+                </p>
+              </div>
+              <p className=" text-[#757575] text-xs md:text-base">1 of 8</p>
+            </div>
+            <a
+              style={{
+                fontFamily: "DM Sans, sans-serif",
+                transition: "transform 0.2s ease-in-out",
+              }}
+              className=" btn-second-main mt-6"
+            >
+              Place a bid
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* //2nd card */}
+      <div className="px-10 md:px-0 mt-5">
+        <div className="flex gap-2">
+          <img src={ban2} alt="" />
+          <div
+            style={{
+              fontFamily: "DM Sans, sans-serif",
+            }}
+            className=""
+          >
+            <h1 className=" font-bold text-xl">The Futr Abstr</h1>
+            <div className="flex items-center gap-2 mt-3">
+              <img src={people} alt="" />
+              <div className="flex gap-1 border-[1px] border-[#00AC4F] p-1 rounded-md">
+                <img src={col2logo} alt="" />
+                <p className=" text-[#00AC4F] font-bold text-xs md:text-base">
+                  0.25 ETH
+                </p>
+              </div>
+              <p className=" text-[#757575] text-xs md:text-base">1 of 8</p>
+            </div>
+            <a
+              style={{
+                fontFamily: "DM Sans, sans-serif",
+                transition: "transform 0.2s ease-in-out",
+              }}
+              className=" btn-second-main mt-6"
+            >
+              Place a bid
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* 3rd card */}
+      <div className="px-10 md:px-0 mt-5">
+        <div className="flex gap-2">
+          <img src={ban2} alt="" />
+          <div
+            style={{
+              fontFamily: "DM Sans, sans-serif",
+            }}
+            className=""
+          >
+            <h1 className=" font-bold text-xl">The Futr Abstr</h1>
+            <div className="flex items-center gap-2 mt-3">
+              <img src={people} alt="" />
+              <div className="flex gap-1 border-[1px] border-[#00AC4F] p-1 rounded-md">
+                <img src={col2logo} alt="" />
+                <p className=" text-[#00AC4F] font-bold text-xs md:text-base">
+                  0.25 ETH
+                </p>
+              </div>
+              <p className=" text-[#757575] text-xs md:text-base">1 of 8</p>
+            </div>
+            <a
+              style={{
+                fontFamily: "DM Sans, sans-serif",
+                transition: "transform 0.2s ease-in-out",
+              }}
+              className=" btn-second-main mt-6"
+            >
+              Place a bid
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SecondColum;
--- src/pages/home/topCollection/TopCollection.jsx
+++ src/pages/home/topCollection/TopCollection.jsx
@@ -5,7 +5,7 @@ import SecondColum from "./SecondColum";
 const TopCollection = () => {
     return (
         <>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-5 my-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 my-10">
             <div><FirstCollum></FirstCollum></div>
             <div><SecondColum></SecondColum></div>
             <div>3rd</div>
