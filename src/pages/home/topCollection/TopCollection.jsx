73489c1cc858af023dbd0ac55fabca0825b5c5f2
Ashik465 <helloashikul@gmail.com>
2023-06-28 16:57:00 +0600

    top collection updated

--- src/pages/home/topCollection/TopCollection.jsx
+++ src/pages/home/topCollection/TopCollection.jsx
@@ -5,7 +5,7 @@ import ThirdColum from "./ThirdColum";
 const TopCollection = () => {
   return (
     <>
-      <div className="grid grid-cols-1 p-5 xl:p-0 xl:grid-cols-3 gap-5 my-10 container mx-auto">
+      <div className="grid grid-cols-1 p-2 xl:p-0 xl:grid-cols-3 gap-5 my-10 container mx-auto">
         <div>
           <FirstCollum></FirstCollum>
         </div>
